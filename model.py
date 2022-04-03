fdb0bcbbcda41f7cd38a44988eb2662f18c0e988
chyyen <61071780+chyyen@users.noreply.github.com>
2022-04-04 00:34:08 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,27 +1,46 @@
-class Grid() :
-	def __init__(self, row, col, x_coor, y_coor, width, adj_bomb) :
-		self.row = row
-		self.col = col
-		self.x_coor = x_coor
-		self.y_coor = y_coor
+from pygame.rect import Rect
+from pygame.sprite import Sprite
+from init import mainWindow
+
+class Grid(Sprite) :
+	def __init__(self, x_coor, y_coor, width, adj_bomb, unflipped_sur, flipped_sur, flag_sur, mark_sur) :
+		# (x_coor, y_coor) : coordinate of left top
+		# width : width of the grid
+		# adj_bomb : number of bombs that are adjacent to the grid
+		# {unflipped, flipped, flag, mark}_sur : surface showed when the it is true
+		super(Grid, self).__init__()
+		self.rect = Rect(x_coor, y_coor, width, width)
 		self.width = width
 		self.adj_bomb = adj_bomb # -1 if there is a bomb
 		self.flipped = False
+		self.unflipped_sur = unflipped_sur
+		mainWindow.blit(self.unflipped_sur, self.rect)
+		self.flipped_sur = flipped_sur # if not a bomb, it will be a text that shows {adj_bomb}
+		self.flipped_sur.set_alpha(180)
+		# two tags : question mark (mark) or flag (flag)
 		self.flag = False
-		self.marked = False
-		# two tags : question mark (marked) or flag (flag)
-
-	def __repr__(self) :
-		return f"{self.adj_bomb}"
+		self.flag_sur = flag_sur
+		self.mark = False
+		self.mark_sur = mark_sur
 
 	def flip(self) :
 		self.flipped = True
+		has_tagged = self.flag or self.mark # return if there are tagged
+		if self.adj_bomb == -1 : # if flip a bomb, return -1
+			has_tagged = -1
+		self.flag = False
+		self.mark = False
+		mainWindow.blit(self.flipped_sur, self.rect)
+		return has_tagged
 
-	def add_tag(self) :
+	def change_tag(self) :
+		mainWindow.blit(self.unflipped_sur, self.rect)
 		if self.flag :
 			self.flag = False
-			self.marked = True
-		elif self.marked :
-			self.marked = False
+			self.mark = True
+			mainWindow.blit(self.mark_sur, self.rect)
+		elif self.mark :
+			self.mark = False
 		else :
-			self.flag = True
+			self.flag = True			
+			mainWindow.blit(self.flag_sur, self.rect)
