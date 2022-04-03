ad47e28fa80867953ba8d321295fd06993360df4
chyyen <61071780+chyyen@users.noreply.github.com>
2022-04-04 00:31:16 +0800

    Add files via upload

--- init.py
+++ init.py
@@ -6,4 +6,6 @@ pygame.font.init()
 
 mainWindow = pygame.display.set_mode((1600, 1000)) # main window
 sound = pygame.mixer.Sound("sound/test.wav") # sound effect when fail
-
+# common size of font
+font30 = pygame.font.Font('fonts/ComicRelief.ttf', 30) 
+font60 = pygame.font.Font('fonts/ComicRelief.ttf', 60) 
